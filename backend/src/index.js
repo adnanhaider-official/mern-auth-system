d0b91d0fb416e7b77059f587212af0fd2b1776d6
adnanhaider-official <adnan.haider.tech@gmail.com>
2026-09-19 21:08:16 +0500

    add mongodb connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 import "dotenv/config";
-
+import dns from "dns";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
-const PORT = process.env.PORT || 3000;
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
